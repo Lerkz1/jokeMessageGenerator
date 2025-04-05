fb123bf51d8cfef26889fd8c13eae7d5b59410f6
Lerkz1 <maleekz966@gmail.com>
2025-04-05 13:51:39 +0100

    added a call to formatJoke function to ensure program runs correctly

---
+++ jokeMsgGenerator/jokes.js
@@ -0,0 +1,37 @@
+/*  getting numbers from 0 to 1*/
+function createRandomNumber(num) {
+    return Math.floor(Math.random() * num);
+};
+
+// tellMeAjoke var storing 3 arrays containing words or phrases that will make up the randomised jokes
+const tellMeAJoke = {
+    whoseThere: ['Honey bee', 'Woo', 'Leaf', 'Nana', 'Canoe'],
+    jokePhrase: ['Honey bee a dear and get that bottle for me please!', 'Glad you are excisted, too', 'Leaf me alone!', 'Nana your business', 'Canoe come out now?' ],
+    theLaugh: ['hahahaha', 'huehuehuehue', 'jajajajajaja', 'hehehehehehe', 'mwahahahuehueheehee']
+};
+
+let aFunny = [];
+
+let jokeID = createRandomNumber(5);
+
+for (let knock in tellMeAJoke) {
+    switch(knock) {
+        case 'whoseThere':
+            aFunny.push(`Knock knock, Whose there? "${tellMeAJoke[knock][jokeID]}".`);
+            aFunny.push(`"${tellMeAJoke[knock][jokeID]} ", who?`);
+            break;
+        case 'jokePhrase':
+            aFunny.push(`"${tellMeAJoke[knock][jokeID]}"`);
+            break;
+        case 'theLaugh':
+            aFunny.push(`${tellMeAJoke[knock][jokeID]}`);
+            break;
+    }
+};
+
+function formatJoke(knock) {
+    const addJoke = aFunny.join('\n')
+    console.log(addJoke)
+};
+
+formatJoke(aFunny);
